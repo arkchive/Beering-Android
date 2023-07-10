a8c0da7ebf621c5ececf8319b9ba06411ec11bb3
sh1220 <jiu0009@naver.com>
2023-07-10 16:20:16 +0900

    feature:bottom_navigation

---
+++ app/src/main/java/com/example/beering/MyFragment.kt
@@ -0,0 +1,22 @@
+package com.example.beering
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.beering.databinding.FragmentMyBinding
+
+class MyFragment : Fragment() {
+    lateinit var binding: FragmentMyBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMyBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+}
