fc2cea6a826869ce377ec7d54b7d12fe44c3c38d
BuildTools <unconfigured@null.spigotmc.org>
2024-02-21 13:39:22 +0900

    fix: baseActivity

--- app/src/main/java/com/example/beering/feature/auth/login/LoginActivity.kt
+++ app/src/main/java/com/example/beering/feature/auth/login/LoginActivity.kt
@@ -15,7 +15,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.example.beering.R
 import com.example.beering.feature.MainActivity
-import com.example.beering.util.getRetrofit_sync
+import com.example.beering.util.getRetrofit
 import com.example.beering.databinding.ActivityLoginBinding
 import com.example.beering.feature.auth.join.view.JoinActivity
 import com.example.beering.feature.auth.login.LoginActivity.Constants.TAG
@@ -79,6 +79,12 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        binding.loginLookaroundTv.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
 
 
@@ -115,7 +121,7 @@ class LoginActivity : AppCompatActivity() {
                             // 로그인 성공시에 대한 로직
 
                             //api 연결
-                            val signInService = getRetrofit_sync().create(LoginApiService::class.java)
+                            val signInService = getRetrofit().create(LoginApiService::class.java)
                             val user =
                                 KakaoLoginRequest(token.idToken!!, token.accessToken, token.refreshToken)
                             signInService.kakaoSignIn(user).enqueue(object : retrofit2.Callback<KakaoLoginResponse> {
@@ -317,7 +323,7 @@ class LoginActivity : AppCompatActivity() {
 
     fun singIn() {
         //api 연결
-        val signInService = getRetrofit_sync().create(LoginApiService::class.java)
+        val signInService = getRetrofit().create(LoginApiService::class.java)
         val user =
             LoginRequest(binding.loginIdEd.text.toString(), binding.loginPasswordEd.text.toString())
         signInService.signIn(user).enqueue(object : retrofit2.Callback<LoginResponse> {
